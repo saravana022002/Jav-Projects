9a7903eb6b2b978a1b57e418f066663222864485
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2025-03-16 13:53:14 +0530

    sort 0s, 1s and 2s in an array

--- src/Arrays/Sort0s1s2s.java
+++ src/Arrays/Sort0s1s2s.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class Sort0s1s2s {
 
     public static void main(String[] args) {
-        int[] nums = {0,0};
+        int[] nums = {2,0,2,1,1,0};
         myApproach1(nums);
         System.out.println(Arrays.toString(nums));
     }
@@ -15,55 +15,23 @@ public class Sort0s1s2s {
 
         int startingAddress = 0;
 
-        int loopCount = 0;
+        int midAddress = startingAddress;
         int endingAddress = nums.length-1;
         int temp;
-        while (startingAddress < endingAddress) {
-            if(nums[startingAddress] == 0){
-                startingAddress++;
-            }else if(nums[startingAddress] == 2){
+        while (midAddress <= endingAddress){
+            if(nums[midAddress] == 0){
                 temp = nums[startingAddress];
-                nums[startingAddress] = nums[endingAddress];
-                nums[endingAddress] = temp;
-                endingAddress --;
-            }
-
-            if(nums[endingAddress] == 2){
-                endingAddress--;
-            }else if(nums[endingAddress] == 0){
-                temp = nums[endingAddress];
-                nums[endingAddress] = nums[startingAddress];
-                nums[startingAddress] = temp;
+                nums[startingAddress] = nums[midAddress];
+                nums[midAddress] = temp;
                 startingAddress++;
-            }
-
-            if(startingAddress > endingAddress){
-                break;
-            }
-            if(nums[endingAddress] == 1){
-
-                if(loopCount == nums.length){
-                    break;
-                }
-                if(nums[endingAddress-1] == 2){
-                    temp = nums[endingAddress];
-                    nums[endingAddress] = nums[endingAddress-1];
-                    nums[endingAddress-1] = temp;
-                    endingAddress --;
-                }
-                loopCount ++;
-            }
-            if(nums[startingAddress] == 1){
-                if(loopCount == nums.length){
-                    break;
-                }
-                if(nums[startingAddress+1] == 0) {
-                    temp = nums[startingAddress];
-                    nums[startingAddress] = nums[startingAddress + 1];
-                    nums[startingAddress + 1] = temp;
-                    startingAddress++;
-                }
-                loopCount ++;
+                midAddress++;
+            }else if(nums[midAddress] == 1){
+                midAddress++;
+            } else if(nums[midAddress] == 2){
+                temp = nums[endingAddress];
+                nums[endingAddress] = nums[midAddress];
+                nums[midAddress] = temp;
+                endingAddress --;
             }
         }
     }
