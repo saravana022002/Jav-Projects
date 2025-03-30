ff8d4eac405988c40171b3e7c0f287ca85a01fc5
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2025-03-30 14:27:54 +0530

    find primitive type sprted arrays

--- src/Arrays2/Find1DuplicateAmongNint.java
+++ src/Arrays2/Find1DuplicateAmongNint.java
@@ -7,9 +7,31 @@ public class Find1DuplicateAmongNint {
         int[] arr = {1,3,4,2,2};
         int duplicateWithFrequency = findDuplicateWithFrequency(arr);
         int dupWithSorting = findDuplicateWithSorting(arr);
+        int dupWithNoExtraSpace = findDuplicateWithNoExtraSpace(arr);
+
 
         System.out.println("The Duplicate Element with frequency,"+duplicateWithFrequency);
         System.out.println("The Duplicate Element with sorting,"+dupWithSorting);
+        System.out.println("The Duplicate Element with NoExtraSpace,"+dupWithNoExtraSpace);
+    }
+
+    private static int findDuplicateWithNoExtraSpace(int[] arr) {
+
+        int hare = 0;
+        int turtle = 0;
+        do {
+            turtle = arr[turtle];
+            hare = arr[arr[hare]];
+        } while (turtle != hare);
+
+        hare = 0;
+
+        do {
+            turtle = arr[turtle];
+            hare = arr[hare];
+        } while (turtle != hare);
+
+        return turtle;
     }
 
     private static int findDuplicateWithFrequency(int[] arr) {
