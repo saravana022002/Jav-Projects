b17dcd342031343b9fffdfb3dc603e1944a6df32
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2025-04-12 23:33:55 +0530

    Number Of Beautiful Sub Arrays

---
+++ src/Arryas4/NumberOFBeautifulSubArrays.java
@@ -0,0 +1,24 @@
+package Arryas4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberOFBeautifulSubArrays {
+    public static void main(String[] args) {
+        int[] nums = {4,3,1,2,4};
+        System.out.println(optimalSolution(nums));
+    }
+    public static long optimalSolution(int[] nums){
+        long k = 0;
+        Map<Long, Long> prefixXORFreq = new HashMap<>();
+        prefixXORFreq.put(0L, 1L);
+        long sum = 0;
+        long count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum ^ nums[i];
+            count += prefixXORFreq.getOrDefault(sum ^ k, 0L);
+            prefixXORFreq.put(sum, prefixXORFreq.getOrDefault(sum, 0L) + 1L);
+        }
+        return count;
+    }
+}
