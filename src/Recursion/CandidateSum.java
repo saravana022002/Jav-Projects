4189d2d54e523ebbf2285866f8e81a3ebfbf2e63
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2025-05-02 18:12:52 +0530

    candidate sum

---
+++ src/Recursion/CandidateSum.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CandidateSum {
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        combinationSum(res, ans, candidates, target, 0, 0);
+        System.out.println(res);
+    }
+    public static void combinationSum(List<List<Integer>> res, List<Integer> ans, int[] candidates, int target, int sum, int index) {
+        if(sum >= target || index == candidates.length){
+            if(sum == target){
+                res.add(new ArrayList<>(ans));
+            }
+            return;
+        }
+        ans.add(candidates[index]);
+        combinationSum(res, ans, candidates, target, sum + candidates[index], index);
+        ans.remove(ans.size() - 1);
+        combinationSum(res, ans, candidates, target, sum, index + 1);
+    }
+
+}
