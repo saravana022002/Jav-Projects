99a20cf6d9f6100b203173fd80b95c68be4890aa
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2025-05-07 01:19:58 +0530

    sqrt rat in maze single Non Duplicate Word Break

---
+++ src/RecursionAndBackTracking/WordBreak.java
@@ -0,0 +1,33 @@
+package RecursionAndBackTracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordBreak {
+    public static void main(String[] args) {
+        String s = "catsandog";
+        List<String> wordDict = Arrays.asList("cats","dog","sand","and","cat");
+        System.out.println(wordBreak(s, wordDict, 0, new Boolean[s.length()]));
+    }
+    public static boolean wordBreak(String s, List<String> wordDict, int index, Boolean[] memo) {
+        if(index == s.length()){
+            return true;
+        }
+        if(memo[index] != null){
+            return memo[index];
+        }
+        for (int i = 0; i < wordDict.size(); i++){
+            String compare = wordDict.get(i);
+            if (index + compare.length() <= s.length()) {
+                String subString = s.substring(index, index + compare.length());
+                if (subString.equals(compare)) {
+                    if (wordBreak(s, wordDict, index + compare.length(), memo)) {
+                        return memo[index] = true;
+                    }
+                }
+            }
+        }
+        return memo[index] = false;
+    }
+}
