cc02dc24dfce645d4123b90e337a923d1d7a7d9c
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2026-08-27 20:00:19 +0530

    26 aug

---
+++ src/Leetcode/PrefixSum/LC724FindPivotIndex.java
@@ -0,0 +1,49 @@
+package Leetcode.PrefixSum;
+
+public class LC724FindPivotIndex {
+    public static void main(String[] args) {
+        int[] nums = {0,0};
+        System.out.println(pivotIndexWithoutSpace(nums));
+    }
+    public static int pivotIndex(int[] nums) {
+        int[] ans = new int[nums.length];
+        int count = 0;
+        for(int i = nums.length - 1; i >= 0; i--){
+            count += nums[i];
+            ans[i] = count;
+        }
+
+        int count1 = 0;
+        for(int i = 0; i < nums.length; i++){
+            count1 += nums[i];
+            if(count1 == ans[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // without extra space
+    // Sum = Leftsum + rightSum + num[i]
+    // Sum - Leftsum - num[i] = rightSum
+
+    public static int pivotIndexWithoutSpace(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        int rightSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            rightSum = totalSum - leftSum - nums[i];
+            if(rightSum == leftSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+
+}
---
+++ src/Leetcode/PrefixSum/LC974SubArraysSumDivByK.java
@@ -0,0 +1,30 @@
+package Leetcode.PrefixSum;
+
+import java.util.HashMap;
+
+public class LC974SubArraysSumDivByK {
+    public static void main(String[] args) {
+        int[] nums = {4,5,0,-2,-3,1};
+        int k = 5;
+        System.out.println(subArraysDivByK(nums, k));
+    }
+
+
+
+    // p[r] - p[l] = t ; where t is divisible by k
+    // in Modulo arithmetic it is, p[r] - p[l] =_= 0 (mod K)
+    // (Mod k)p[r] = p[l] (Mod k)
+    public static int subArraysDivByK(int[] nums, int k) {
+        int[] map = new int[nums.length];
+        map[0] = 1;
+        int count = 0;
+        int ans = 0;
+        for (int num : nums) {
+            count += num;
+            int remainder = ((count % k) + k) % k;
+            ans += map[remainder];
+            map[remainder]++;
+        }
+        return ans;
+    }
+}
