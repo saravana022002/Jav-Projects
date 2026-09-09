048f6574e1a0df4191b451e3f5e699dbf11d7b1f
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2026-09-09 22:53:18 +0530

    Lookup fix

---
+++ src/Leetcode/HashMapAsLookup/LC49GroupAnagrams.java
@@ -0,0 +1,28 @@
+package Leetcode.HashMapAsLookup;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LC49GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] arr = new char[26];
+            for (int j = 0; j < s.length(); j++) {
+                arr[s.charAt(j) - 'a']++;
+            }
+
+            String key = new String(arr);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
---
+++ src/Leetcode/HashMapAsLookup/LC974SubarraysDivByK.java
@@ -0,0 +1,24 @@
+package Leetcode.HashMapAsLookup;
+
+public class LC974SubarraysDivByK {
+    public static void main(String[] args) {
+        int[] nums = {4,5,0,-2,-3,1};
+        int k = 5;
+        System.out.println(subarraysDivByK(nums, k));
+    }
+
+    public static int subarraysDivByK(int[] nums, int k) {
+        int[] rems = new int[k];
+        rems[0] = 1;
+        int count = 0;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+            int rem = ((sum % k) + k) % k;
+            count += rems[rem];
+            rems[rem]++;
+        }
+        return count;
+    }
+
+}
