f6b4553b04acb4e18c11a26bd6b6cd08204b89d6
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2026-09-17 10:47:29 +0530

    codes

--- src/Leetcode/HashMapAsLookup/LC523ContinuousSubArraySum.java
+++ src/Leetcode/HashMapAsLookup/LC523ContinuousSubArraySum.java
@@ -1,18 +1,15 @@
-package Leetcode.PrefixSum;
+package Leetcode.HashMapAsLookup;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-public class LC523ContinuousSubarraySum {
+public class LC523ContinuousSubArraySum {
+
     public static void main(String[] args) {
         int[] nums = {23,2,4,6,7};
         int k = 6;
-        System.out.println(checkSubarraySum(nums, k));
+        checkSubarraySum(nums, k);
     }
-
-    // p[r] - p[l] = x , // where t is multiple of k , //x = n * k , n can be 0 also, r - l >= 2
     public static boolean checkSubarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
