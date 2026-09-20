4ced6d6c81af6c1988d65c5070f2233985091c64
SaravanaSankar <72879209+saravana022002@users.noreply.github.com>
2026-09-20 17:28:47 +0530

    Mulitset Rasoning

---
+++ src/Leetcode/FrequencyAndMultisetResoning/LC383RansomNote.java
@@ -0,0 +1,23 @@
+package Leetcode.FrequencyAndMultisetResoning;
+
+public class LC383RansomNote {
+    public static void main(String[] args) {
+        String ransomNote = "a", magazine = "b";
+        canConstruct(ransomNote, magazine);
+    }
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        char[] charm = ransomNote.toCharArray();
+        for (char c : charm) {
+            count[c - 'a']++;
+        }
+        char[] chars = ransomNote.toCharArray();
+        for(int i = 0; i < ransomNote.length(); i++){
+            if(count[chars[i] - 'a'] == 0){
+                return false;
+            }
+            count[chars[i] - 'a']--;
+        }
+        return true;
+    }
+}
---
+++ src/Leetcode/FrequencyAndMultisetResoning/LC49GroupAnagrams.java
@@ -0,0 +1,22 @@
+package Leetcode.FrequencyAndMultisetResoning;
+
+import java.util.*;
+
+public class LC49GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
